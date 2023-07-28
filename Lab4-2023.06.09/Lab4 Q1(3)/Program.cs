7700df7f1781439b61be18da80b9146c58b38f22
DLSNemsara <137623794+DLSNemsara@users.noreply.github.com>
2023-07-28 08:12:28 +0530

    Add files via upload

---
+++ Lab4-2023.06.09/Lab4 Q1(3)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create an object of the ConvertValues class
+        ConvertValues converter = new ConvertValues();
+
+        // Get the user input for kilometers
+        Console.Write("Enter the distance in kilometers (km): ");
+        if (double.TryParse(Console.ReadLine(), out double kilometers))
+        {
+            // Call the kilometerToMeter method with the user input value
+            double meters = converter.KilometerToMeter(kilometers);
+            Console.WriteLine($"The distance in meters (m) is: {meters} m");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid numeric value for kilometers.");
+        }
+
+        Console.ReadLine();
+    }
+}
+
+class ConvertValues
+{
+    public double KilometerToMeter(double kilometers)
+    {
+        double meters = kilometers * 1000;
+        return meters;
+    }
+}
